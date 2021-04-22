a37233c9fbe2d9083f0662a68daba911db46608e
donghao <jdh.workuse@gmail.com>
2021-04-22 22:24:39 +0800

    first commit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->call('App\Http\Controllers\ConstellationController@store')->hourly();
+        $schedule->call('App\Http\Controllers\ConstellationController@store')->everyMinute();
 
     }
 
